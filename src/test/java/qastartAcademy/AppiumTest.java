8e054cdb18bf798d9cd0054b482e165554e67560
Jeevak <mishrajeevak@gmail.com>
2021-04-19 15:10:15 +0530

    first commit

---
+++ src/test/java/qastartAcademy/AppiumTest.java
@@ -0,0 +1,19 @@
+package qastartAcademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	
+	@Test
+	public void NativeAppAndroid()
+	{
+		System.out.println("nativeAndroid");
+	}
+	
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("IOSApp");
+	}
+}
